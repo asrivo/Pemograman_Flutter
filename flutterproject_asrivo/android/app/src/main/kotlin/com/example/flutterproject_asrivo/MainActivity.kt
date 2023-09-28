3d0a47c1726d8720b98cc21ae63f9469bb0c6b53
Asrivo Maha Kurnia Pitama <asrivomkp@gmail.com>
2023-09-28 10:57:48 +0700

    upload project flutterproject_asrivo, video nomor 8 List Tile

---
+++ flutterproject_asrivo/android/app/src/main/kotlin/com/example/flutterproject_asrivo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterproject_asrivo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutterproject_asrivo/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List Tile"),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 8,
+                ),
+              title: Text("Asrivo Maha Kurnia Pitama"),
+              subtitle: Text(
+                "This is subtitle okay.. aabawufgdjniwutfuhbcwirtfgshjbgfhcvmoi2yegwhd",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text("10.00 PM"),
+              // tileColor: Colors.amber,
+              // dense: true,
+              // onTap: () {
+              //   return;
+              // },
+            ),
+
+            Divider(
+              color: Colors.black,
+            ),
+
+            ListTile(
+              title: Text("Asrivo Maha Kurnia Pitama"),
+              subtitle: Text("This is subtitle okay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10.00 PM"),
+              tileColor: Colors.green,
+            ),
+
+            Divider(
+              color: Colors.black,
+            ),
+
+            ListTile(
+              title: Text("Asrivo Maha Kurnia Pitama"),
+              subtitle: Text("This is subtitle okay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10.00 PM"),
+              tileColor: Colors.blueAccent,
+            ),
+
+            Divider(
+              color: Colors.black,
+            ),
+
+            ListTile(
+              title: Text("Asrivo Maha Kurnia Pitama"),
+              subtitle: Text("This is subtitle okay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10.00 PM"),
+              tileColor: Colors.redAccent,
+            ),
+
+            Divider(
+              color: Colors.black,
+            ),
+
+            ListTile(
+              title: Text("Asrivo Maha Kurnia Pitama"),
+              subtitle: Text("This is subtitle okay.."),
+              leading: CircleAvatar(),
+              trailing: Text("10.00 PM"),
+              tileColor: Colors.pinkAccent,
+            ),
+          ],
+        ),
+        ),
+    );
+  }
+}
