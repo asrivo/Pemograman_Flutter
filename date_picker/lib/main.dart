3b4d8fcddfdd640a87d7a9f5cdb1b6f3f127e6a2
Asrivo Maha Kurnia Pitama <asrivomkp@gmail.com>
2023-11-08 17:45:54 +0700

    Upload FLUTTER 34, Date Picker

---
+++ date_picker/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime selectDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Date Picker"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              selectDate.toString(),
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {
+                showDatePicker(
+                  context: context,
+                  initialDate: selectDate,
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2025),
+                  // selectableDayPredicate: (DateTime day) {
+                  //   return day.isAfter(DateTime.now().subtract(Duration(days: 5)) &&
+                  //       day.isBefore(DateTime.now().add(Duration(days: 2)));
+                  // },
+                  helpText: "Help Text",
+                  cancelText: "Cancel Text",
+                  confirmText: "Confirm Text",
+                  fieldHintText: "Masukan tanggal lahir anda",
+                  fieldLabelText: "Field Label Text",
+                  builder: (context, child) {
+                    return Theme(
+                      data: ThemeData.light(),
+                      child: child!,
+                    );
+                  },
+                ).then((value) {
+                  if (value != null) {
+                    setState(() {
+                      selectDate = value;
+                    });
+                  }
+                });
+              },
+              child: Text("Date Picker"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
